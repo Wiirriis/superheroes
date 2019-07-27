248e361544ebcf1c6a6d977ac516f5a1fb27a438
César Polanco <cpolcam@outlook.com>
2019-07-27 15:29:08 -0600

    Modificaciones

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,13 +11,18 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
 
-  constructor(private _heroesService: HeroesService ) { 
+  constructor(private _heroesService: HeroesService,
+              private router:Router) { 
 
   }
 
   ngOnInit() {
       this.heroes = this._heroesService.getHeroes();
       console.log(this.heroes);
+  }
+
+  verHeroe(idx:number){
+    this.router.navigate(['/heroe',idx]);
   }
 
 }
