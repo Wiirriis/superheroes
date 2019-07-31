5fda8a8ccb20844fd905b01ce0b4eb0cbced04be
César Polanco <cpolcam@outlook.com>
2019-07-31 11:03:54 -0600

    Modifications added

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HeroesService } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -11,11 +12,18 @@ export class HeroeComponent {
   heroe:any=[];
 
   constructor(private activatedRoute:ActivatedRoute,
-              private heroesService:HeroesService) {
+              private heroesService:HeroesService,
+              private router:Router) {
     this.activatedRoute.params.subscribe(params => {
       this.heroe = this.heroesService.getHeroe(params['id']);
+      console.log(this.heroe);
     })
     
+  }
+
+
+  volver(){
+    this.router.navigate(['heroes']);
   }
 
 
